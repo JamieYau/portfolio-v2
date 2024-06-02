447588aad8b49e76e47fbe065854864da60474da
JamieYau <jamie.yau2002@me.com>
2024-06-02 02:53:04 +0100

    Adjust header colors to use theme variables

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,15 +14,15 @@ export default function Header() {
   return (
     <header className="flex w-full items-center justify-center p-8">
       <div className="flex-1"></div>
-      <NavigationMenu className="rounded-2xl bg-muted p-4">
-        <NavigationMenuList>
-          <NavigationMenuItem className="bg-slate-700">
+      <NavigationMenu className="rounded-3xl bg-muted p-4">
+        <NavigationMenuList className="gap-4 text-sm text-muted-foreground">
+          <NavigationMenuItem className="hover:text-foreground">
             <NavigationMenuLink href="/#about">01. About</NavigationMenuLink>
           </NavigationMenuItem>
-          <NavigationMenuItem className="bg-slate-700">
+          <NavigationMenuItem className="hover:text-foreground">
             <NavigationMenuLink href="/#about">02. Projects</NavigationMenuLink>
           </NavigationMenuItem>
-          <NavigationMenuItem className="bg-slate-700">
+          <NavigationMenuItem className="hover:text-foreground">
             <NavigationMenuLink href="/#about">03. Contact</NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>

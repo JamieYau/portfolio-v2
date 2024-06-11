d4477bd63d7cbc5b7cdaba801bdc0bbf057b62fc
JamieYau <jamie.yau2002@me.com>
2024-06-11 06:10:38 +0100

    Remove redundant element

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -33,9 +33,11 @@ export default function Hero() {
           className="text-[clamp(40px,8vw,5em)] leading-tight"
           delay={introTypingDuration * 1000 + 500} // Adjust delay to match the timing of the first line
         />
-        <h3 className="text-[clamp(30px,6vw,4em)] font-semibold leading-tight">
-          <Highlight> Full Stack Developer</Highlight>
-        </h3>
+
+        <Highlight className="text-[clamp(30px,5vw,4em)] font-semibold leading-tight">
+          Full Stack Developer
+        </Highlight>
+
         <p className="mt-4 max-w-lg text-muted-foreground">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam nihil
           asperiores quis, iure magni aut omnis labore qui iste architecto

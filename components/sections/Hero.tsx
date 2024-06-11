b4b2f32b78b87e88080700797c3f451f12d79b9e
JamieYau <jamie.yau2002@me.com>
2024-06-11 06:50:40 +0100

    Add animations to other sections of hero to fit

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { HeroHighlight, Highlight } from "../ui/hero-highlight";
 import { TypewriterEffect } from "../ui/typewriter-effect";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   const intro = [
@@ -22,28 +23,54 @@ export default function Hero() {
         containerClassName="flex flex-col justify-center"
         className="flex flex-col items-start px-8"
       >
-        <TypewriterEffect
-          words={intro}
-          className="font-medium text-primary"
-          cursorClassName="bg-primary"
-          hideCursorAfterTyping={true}
-        />
-        <TypewriterEffect
-          words={name}
-          className="text-[clamp(40px,8vw,5em)] leading-tight"
-          delay={introTypingDuration * 1000 + 500} // Adjust delay to match the timing of the first line
-        />
-
-        <Highlight className="text-[clamp(30px,5vw,4em)] font-semibold leading-tight">
-          Full Stack Developer
-        </Highlight>
-
-        <p className="mt-4 max-w-lg text-muted-foreground">
+        <h3 className="flex">
+          <TypewriterEffect
+            words={intro}
+            className="font-medium text-primary"
+            cursorClassName="bg-primary"
+            hideCursorAfterTyping={true}
+          />
+        </h3>
+        <h1 className="flex">
+          <TypewriterEffect
+            words={name}
+            className="text-[clamp(40px,8vw,5em)] leading-tight"
+            delay={introTypingDuration * 1000 + 500} // Adjust delay to match the timing of the first line
+          />
+        </h1>
+        <motion.h2
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 2,
+            ease: "linear",
+            delay: 3,
+          }}
+        >
+          <Highlight className="text-[clamp(30px,5vw,4em)] font-semibold leading-tight">
+            Full Stack Developer
+          </Highlight>
+        </motion.h2>
+        <motion.p
+          // reveals content from left to right
+          initial={{ clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)" }}
+          whileInView={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 1.5,
+            delay: 5,
+          }}
+          className="mt-4 max-w-lg text-muted-foreground"
+        >
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam nihil
           asperiores quis, iure magni aut omnis labore qui iste architecto
           pariatur doloremque, commodi incidunt a natus libero non accusantium
           animi.
-        </p>
+        </motion.p>
       </HeroHighlight>
     </section>
   );

9f40a6eed1b284ccc152b255327416b66a8a0865
JamieYau <jamie.yau2002@me.com>
2024-06-11 19:32:31 +0100

    Add Carousel to skills section

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -1,11 +1,15 @@
+"use client";
 import { skills } from "@/lib/data";
+import { Card, CardContent } from "@/components/ui/card";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import React from "react";
 
 export default function Skills() {
   return (
@@ -17,24 +21,56 @@ export default function Skills() {
         <span className="mr-4">Skills</span>
       </h2>
       <div>
-        {skills.map((skillCategory) => (
-          <Card className="mb-8" key={skillCategory.category}>
-            <CardHeader>
-              <CardTitle>{skillCategory.category}</CardTitle>
-            </CardHeader>
-            <CardContent className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-              {skillCategory.items.map((skill) => (
-                <div className="flex items-center space-x-2" key={skill.label}>
-                  <skill.icon
-                    className="h-6 w-6 lg:h-10 lg:w-10"
-                    style={{ fill: skill.color }}
-                  />
-                  <span className="">{skill.label}</span>
-                </div>
-              ))}
-            </CardContent>
-          </Card>
-        ))}
+        {skills.map((skillCategory) => {
+          // Create a new Autoplay instance for each carousel
+          const plugin = Autoplay({
+            delay: 3000,
+            stopOnInteraction: true,
+            stopOnFocusIn: false,
+          });
+
+          return (
+            <div
+              className="mb-8 flex flex-col items-center"
+              key={skillCategory.category}
+            >
+              <div className="mb-8 w-full text-start">
+                <h3 className="text-2xl font-bold">{skillCategory.category}</h3>
+              </div>
+              <Carousel
+                plugins={[plugin]} // Use the unique plugin instance here
+                opts={{
+                  align: "start",
+                  loop: true,
+                }}
+                className="w-full max-w-4xl"
+              >
+                <CarouselContent>
+                  {skillCategory.items.map((skill) => (
+                    <CarouselItem
+                      key={skill.label}
+                      className="sm:basis-1/4 md:basis-1/5"
+                    >
+                      <div>
+                        <Card>
+                          <CardContent className="flex aspect-square flex-col items-center justify-center px-5 py-6 gap-2">
+                            <skill.icon
+                              className="h-6 w-6 lg:h-10 lg:w-10"
+                              style={{ fill: skill.color }}
+                            />
+                            <span className="font-medium leading-5 text-center">{skill.label}</span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+              </Carousel>
+            </div>
+          );
+        })}
       </div>
     </section>
   );

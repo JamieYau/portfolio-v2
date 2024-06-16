6745c482383992a328d457f29d336b13e403cf1a
JamieYau <jamie.yau2002@me.com>
2024-06-16 04:13:06 +0100

    Change arrow icon to bolder svg

--- components/icons/ArrowIcon.tsx
+++ components/icons/ArrowIcon.tsx
@@ -4,9 +4,9 @@ export default function ArrowIcon() {
   return (
     <motion.svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
       width="1em"
       height="1em"
+      viewBox="0 0 256 256"
       animate={{ y: [0, 5, 0] }}
       transition={{
         duration: 2,
@@ -18,7 +18,7 @@ export default function ArrowIcon() {
     >
       <path
         fill="currentColor"
-        d="m205.66 149.66l-72 72a8 8 0 0 1-11.32 0l-72-72a8 8 0 0 1 11.32-11.32L120 196.69V40a8 8 0 0 1 16 0v156.69l58.34-58.35a8 8 0 0 1 11.32 11.32"
+        d="m208.49 152.49l-72 72a12 12 0 0 1-17 0l-72-72a12 12 0 0 1 17-17L116 187V40a12 12 0 0 1 24 0v147l51.51-51.52a12 12 0 0 1 17 17Z"
       ></path>
     </motion.svg>
   );

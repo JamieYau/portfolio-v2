d00003ea85f365747835b99f6d6c834903fae996
JamieYau <jamie.yau2002@me.com>
2024-06-18 21:21:58 +0100

    Remove unused components

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import About from "@/components/sections/About";
 import Contact from "@/components/sections/Contact";
 import Hero from "@/components/sections/Hero";
 import Projects from "@/components/sections/Projects";
-import Skills from "@/components/sections/Skills";
 
 export default function Home() {
   return (

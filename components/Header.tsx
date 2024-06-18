42cb537265597859254ba26701e2d6051b4d6399
JamieYau <jamie.yau2002@me.com>
2024-06-18 04:49:38 +0100

    Add reveal animation for toggle theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,19 @@
 import { ModeToggle } from "./ui/toggle-mode";
 import { navItems } from "@/lib/data";
 import { FloatingNav } from "./ui/floating-navbar";
-
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
     <>
       <FloatingNav navItems={navItems} />
-      <div className="hidden fixed top-12 right-10 sm:block z-50">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        className="fixed right-10 top-12 z-50 hidden sm:block"
+      >
         <ModeToggle />
-      </div>
+      </motion.div>
     </>
   );
 }

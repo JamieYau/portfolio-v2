fed54bd4b8b55d1ab92db7664cae7a54f458d62e
JamieYau <jamie.yau2002@me.com>
2024-06-19 20:58:33 +0100

    Change opacity of nav, to adjust for not being hidden on scroll

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -64,7 +64,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "fixed inset-x-0 top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center rounded-full border border-transparent bg-background px-5 py-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2]",
+          "fixed inset-x-0 top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center rounded-full border border-transparent bg-background/[0.9] px-5 py-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2]",
           className,
         )}
       >

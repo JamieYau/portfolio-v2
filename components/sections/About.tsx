f2cc7f2656607cce5766658fdd821827bc14cfab
JamieYau <jamie.yau2002@me.com>
2024-06-20 06:15:21 +0100

    Fix About Me spacing

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -8,14 +8,18 @@ export default function About() {
   const { ref } = useSectionInView("About", 0.5);
 
   return (
-    <section ref={ref} id="about" className="mx-auto flex w-full flex-col pt-24">
+    <section
+      ref={ref}
+      id="about"
+      className="mx-auto flex w-full flex-col pt-24"
+    >
       <h2 className="mb-4 flex items-center text-[clamp(26px,5vw,2em)] font-semibold before:mr-2 before:text-primary before:content-['01.'] after:h-px after:flex-1 after:bg-muted">
         <span className="mr-4">About Me</span>
       </h2>
-      <div className="flex w-full flex-wrap lg:flex-nowrap">
+      <div className="flex w-full flex-col lg:flex-row gap-4">
         <div className="max-w-2xl">
           <h3 className="text-xl font-semibold">About</h3>
-          <p className="mb-4">
+          <p className="mb-4 tracking-tight">
             I&apos;m a passionate full stack developer that likes creating
             Responsive and User-friendly web applications. My journey began with
             Python, evolving from simple projects to complex games. With a solid
@@ -24,13 +28,13 @@ export default function About() {
             develop full stack web applications.
           </p>
           <h3 className="mt-8 text-xl font-semibold">Education</h3>
-          <p className="mb-4">
+          <p className="mb-4 tracking-tight">
             I hold a Bachelors of Science in Computer Science from Royal
             Holloway University of London, where I gained a strong foundation in
             software development and computer science principles.
           </p>
           <h3 className="mt-8 text-xl font-semibold">Personal Interests</h3>
-          <p className="mb-4">
+          <p className="mb-4 tracking-tight">
             Beyond coding, I have a keen interest in the latest tech
             innovations. To maintain a healthy balance, I regularly hit the gym
             and enjoy sports such as football, basketball, MMA, boxing, and
